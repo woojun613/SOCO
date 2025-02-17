50955b1a284fb8e775725a9f4409df131406c6c5
장우준 <122261629+woojun613@users.noreply.github.com>
2025-02-17 14:23:57 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
+});
