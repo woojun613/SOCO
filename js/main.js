48645478f8812a3b7d858d9576e542b16065a817
장우준 <122261629+woojun613@users.noreply.github.com>
2025-02-18 21:19:27 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+// 탑으로 이동하는 버튼
+$(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 500) {
+      $('#top-btn').fadeIn();
+    } else {
+      $('#top-btn').fadeOut();
+    } 
+  });
+
+  $("#top-btn").click(function() {
+    $('html, body').animate({
+      scrollTop : 0
+    }, 400);
+      return false;
+  });
+});
+
+// 좋아요 스크랩
 $(function(){
   $(".bookmark").click(function(){
     $(this).toggleClass("bookmark-off")
