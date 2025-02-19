b939748fb58fc01f8bd0ba65e01a6adcec809112
장우준 <122261629+woojun613@users.noreply.github.com>
2025-02-19 23:06:19 +0900

    Add files via upload

---
+++ js/main-game-manag.js
@@ -0,0 +1,10 @@
+// 탭메뉴 이벤트
+$(function(){
+	$('.tabcontent > div').hide();
+	$('.tabnav a').click(function () {
+		$('.tabcontent > div').hide().filter(this.hash).fadeIn();
+		$('.tabnav a').removeClass('active');
+		$(this).addClass('active');
+		return false;
+	}).filter(':eq(0)').click();
+});
